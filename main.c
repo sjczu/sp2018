df2b1f8a37f683fd2030f2ebd7eee6d43d210057
sjczu <sadoxz05@gmail.com>
2018-10-27 11:05:02 +0200

    UPDATED main.c

--- main.c
+++ main.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 int main()
 {
-    srand(time(NULL));
-    printf("random=%d",rand());
-    return 0;
+    int dim;
+    int* fib;
+    printf("How many fibonacci numbers u want to generate?");
+    scanf("%d",&dim);
+    fib=(int*)malloc(dim*sizeof(int));
+    if(fib=NULL)
+    {
+        printf("ERROR");
+        return EXIT_FAILURE;
+    }
+    
+    int i;
+    for(i=0;i<dim;i++)
+    {
+        fib[0]=1;
+        fib[1]=1;
+        fib[i+2]=fib[i+1]+fib[i];
+        printf("%d",fib[i]);
+    }
+    free(fib);
+    return EXIT_SUCCESS;
 }
