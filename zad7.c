3b7264adaf30b9b682b57aa0da4178fb4f0d702f
sjczu <sadoxz05@gmail.com>
2018-11-03 11:17:37 +0100

    UPDATED zad7.c

--- zad7.c
+++ zad7.c
@@ -2,18 +2,32 @@
 #include <stdlib.h>
 #include <math.h>
 
-void load_data(int* a)
-{
-  printf("x=");
-  scanf("%d",a);
-}
+//lista fcji
+void load_data(int* a);
+void print_data(int a, int b, float c);
 
+//fcja glowna
 int main()
 {
   int x;
   load_data(&x);
   float pierw=sqrt(x);
   int y=-x;
-  printf("Odwrotnosc=%d\nPierwiastek=%.2f\n",y,pierw);
+  print_data(x,y,pierw);
   return 0;
+}
+
+//wczytywanie danych
+void load_data(int* a)
+{
+  printf("x=");
+  scanf("%d",a);
+}
+
+//wypisywanie danych
+void print_data(int a, int b, float c)
+{
+  if(a>0){printf("Odwrotnosc=%d\nPierwiastek=%.2f\n",b,c);}
+  else if(a<0){printf("Odwrotnosc=%d\nPierwiastek=ERROR(x must be higher than 0)\n",b);}
+  else printf("Odwrotnosc=ERROR(x must be different from 0)\nPierwiastek=%.2f\n",c);
 }
