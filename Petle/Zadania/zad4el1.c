ac146e519182e17b417cd02c4610ef0218711a34
sjczu <sadoxz05@gmail.com>
2018-11-24 08:35:15 +0100

    UPDATED zad4el1.c

---
+++ Petle/Zadania/zad4el1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+  int x;
+  printf("Podaj liczbe naturalna (max=5): ");
+  scanf("%d",&x);
+  if(x>5){printf("ERROR:Za duza liczba\n");}
+  else{
+  int i;
+  int y;
+  for(i=0;i<x;i++)
+  {
+    if(x==1&&x==0)y=x;
+    else if(x==2)y=x*(x-1);
+    else if(x==3)y=x*(x-1)*(x-2);
+    else if(x==4)y=x*(x-1)*(x-2)*(x-3);
+    else y=x*(x-1)*(x-2)*(x-3)*(x-4);
+  }
+  printf("%d\n",y);
+  return 0;}
+}
