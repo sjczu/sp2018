08b6bbc898a7cb6ed90540cfdad8747f34e1bed8
sjczu <sadoxz05@gmail.com>
2018-11-24 08:59:13 +0100

    ADDED DESCRIPTION to zad4el1.c

--- Petle/Zadania/zad4el1.c
+++ Petle/Zadania/zad4el1.c
@@ -2,25 +2,32 @@
 #include <stdlib.h>
 #include <math.h>
 
-//Wypisuje silnie liczby <= 5
+//Wypisuje silnie liczby 0<x<5
 
 int main()
 {
   int x;
   printf("Podaj liczbe naturalna (max=5): ");
   scanf("%d",&x);
-  if(x>5){printf("ERROR:Za duza liczba\n");}
-  else{
-  int i;
-  int y;
-  for(i=0;i<x;i++)
+  
+  if(x>5||x<0){printf("ERROR:Błędna liczba\n");}
+  else
   {
-    if(x==1&&x==0)y=x;
-    else if(x==2)y=x*(x-1);
-    else if(x==3)y=x*(x-1)*(x-2);
-    else if(x==4)y=x*(x-1)*(x-2)*(x-3);
-    else y=x*(x-1)*(x-2)*(x-3)*(x-4);
+    int i;
+    int y;
+    if(x==0)y=0;
+    else if(x==1)y=x;
+    else 
+    {
+      for(i=0;i<x;i++)
+      {
+        if(x==2)y=x*(x-1);
+        else if(x==3)y=x*(x-1)*(x-2);
+        else if(x==4)y=x*(x-1)*(x-2)*(x-3);
+        else y=x*(x-1)*(x-2)*(x-3)*(x-4);
+      }
+    }
+    printf("%d\n",y);
   }
-  printf("%d\n",y);
-  return 0;}
+  return 0;
 }
